3c8cd1cdb2c4bf6472c809325c56d702b115e586
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-28 02:41:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   empty_hash = nil
   found_key = ""
-  top = 606060606060
+  top = name_hash.count
   if name_hash.size != 0
       name_hash.each do |name, number|
         if number < top
