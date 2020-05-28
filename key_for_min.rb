3dbfe4815386116e68b44a727e6fbc288dec12a6
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-28 02:14:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if hash.size != 0 do
+    found_key = ""
+    top = 606060606060
+      name_hash.each do |name, number|
+        if number < top do
+          top = number
+          found_key = name
+        end
+      end
+    found_key
+  else
+    empty_hash = "nil"
+    empty_hash
+  end
 end
